573fa2d92175ee3913f6de4a59bade879356485e
skiltion <taeho1517@gmail.com>
2025-04-02 20:49:59 +0900

    database update

--- db_init.js
+++ db_init.js
@@ -30,10 +30,10 @@ db.serialize(() => {
     CREATE TABLE IF NOT EXISTS posts (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       user_id INTEGER NOT NULL,
+      title TEXT NOT NULL,
       content TEXT NOT NULL,
-      recipe TEXT,
       created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-      FOREIGN KEY (user_id) REFERENCES users(id)
+      FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
     )
   `);
 });
