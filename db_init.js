4876ed675e816e850d44dc2dcd9bd218fafb42e6
skiltion <taeho1517@gmail.com>
2025-04-03 20:47:29 +0900

    database update

--- db_init.js
+++ db_init.js
@@ -36,6 +36,19 @@ db.serialize(() => {
       FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
     )
   `);
+  
+  db.run(`
+    CREATE TABLE IF NOT EXISTS comments (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    post_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    content TEXT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+)
+`);
 });
+  
 
 db.close();
